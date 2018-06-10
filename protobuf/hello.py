900847d674a7f683377b33d2084df61a1cfb37fa
wangdao8023 <wangdao8023@foxmail.com>
2018-06-10 23:58:34 +0800

    init

---
+++ protobuf/hello.py
@@ -0,0 +1,14 @@
+
+import Register_pb2 as register
+
+
+print("wangdaozhi")
+print("10001")
+
+register_ = register.MobileRegister()
+
+register_.uid = "wwfdfadfawngdao802300001"
+
+print(register_)
+
+
